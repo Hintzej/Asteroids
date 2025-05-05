f77cf7d340f151cfff04c5be138f537065ba9f3e
Jordan <42985531+Hintzej@users.noreply.github.com>
2025-05-05 07:38:03 -0600

    Made movable ship

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    main_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for updater in updatable:
+            updater.draw(screen)
+
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
